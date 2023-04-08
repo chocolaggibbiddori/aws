9fc5fcf512678a0e901b5871c188622f632cd3c2
chocolaggibbiddori <didrkd684@naver.com>
2023-04-08 23:53:00 +0900

    Feat and Test : 게시글 등록

---
+++ src/main/java/selfstudy/aws/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package selfstudy.aws.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import selfstudy.aws.service.PostsService;
+import selfstudy.aws.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
